f6b9274ed4293a77c0b2668d7fc69878381e4921
Tom Woodward <twwoodward@me.com>
2020-08-09 15:35:27 -0400

    acf fix save/load, add student cpt

--- inc/acf.php
+++ inc/acf.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 //ACF SAVE and LOAD JSON
 add_filter('acf/settings/save_json', 'suport_matrix_json_save_point');
  
-function alt_ee_json_save_point( $path ) {
+function suport_matrix_json_save_point( $path ) {
     
     // update path
     $path = get_stylesheet_directory() . '/acf-json';
@@ -25,7 +25,7 @@ function alt_ee_json_save_point( $path ) {
 
 add_filter('acf/settings/load_json', 'suport_matrix_json_load_point');
 
-function alt_ee_json_load_point( $paths ) {
+function suport_matrix_json_load_point( $paths ) {
     
     // remove original path (optional)
     unset($paths[0]);
@@ -38,3 +38,70 @@ function alt_ee_json_load_point( $paths ) {
     return $paths;
     
 }
+
+
+//student custom post type
+
+// Register Custom Post Type student
+// Post Type Key: student
+
+function create_student_cpt() {
+
+  $labels = array(
+    'name' => __( 'Students', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Student', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Student', 'textdomain' ),
+    'name_admin_bar' => __( 'Student', 'textdomain' ),
+    'archives' => __( 'Student Archives', 'textdomain' ),
+    'attributes' => __( 'Student Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Student:', 'textdomain' ),
+    'all_items' => __( 'All Students', 'textdomain' ),
+    'add_new_item' => __( 'Add New Student', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Student', 'textdomain' ),
+    'edit_item' => __( 'Edit Student', 'textdomain' ),
+    'update_item' => __( 'Update Student', 'textdomain' ),
+    'view_item' => __( 'View Student', 'textdomain' ),
+    'view_items' => __( 'View Students', 'textdomain' ),
+    'search_items' => __( 'Search Students', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into student', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this student', 'textdomain' ),
+    'items_list' => __( 'Student list', 'textdomain' ),
+    'items_list_navigation' => __( 'Student list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Student list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'student', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array(),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-universal-access-alt',
+  );
+  register_post_type( 'student', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_student_cpt', 0 );
