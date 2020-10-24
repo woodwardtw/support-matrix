1f20a216d47cb4a6d927c668b77c7c79ee143eca
woodwardtw <twwoodward@me.com>
2020-10-24 08:12:37 -0400

    better id structure, clean up names

--- loop-templates/student-form.php
+++ loop-templates/student-form.php
@@ -1,21 +1,22 @@
-<h3> <?php echo get_the_title($sub_post_id);?> </h3>
+<h3> <?php echo get_the_title($lecture_post_id);?> </h3>
 <?php 
-      if( have_rows('assignments', $sub_post_id) ):
-            while( have_rows('assignments', $sub_post_id) ) : the_row();
-                  if(get_sub_field('assignment_name',$sub_post_id) != ""){
-                        $assignment_name = get_sub_field('assignment_name',$sub_post_id);
+      if( have_rows('assignments', $lecture_post_id) ):
+            while( have_rows('assignments', $lecture_post_id) ) : the_row();
+                  if(get_sub_field('assignment_name',$lecture_post_id) != ""){
+                        $assignment_name = get_sub_field('assignment_name',$lecture_post_id);
+                        $assignment_slug = sanitize_title($assignment_name);
                         echo '<h4>' . $assignment_name . '</h4>';
                         echo "<div class=\"input-piece\">
-                                    <input type=\"radio\" id=\"{$sub_slug}-need-help\" name=\"{$assignment_name}\" value=\"need-help\">
-                                    <label class=\"better-label help\" for=\"{$sub_slug}-need-help\">Need Help</label>
+                                    <input type=\"radio\" id=\"{$lecture_slug}-{$assignment_slug}-need-help\" name=\"{$assignment_name}\" value=\"need-help\">
+                                    <label class=\"better-label help\" for=\"{$lecture_slug}-{$assignment_slug}-need-help\">Need Help</label>
                               </div>
                               <div class=\"input-piece\">
-                                    <input type=\"radio\" id=\"{$sub_slug}-some-concern\" name=\"{$assignment_name}\" value=\"some-concern\">
-                                    <label class=\"better-label concern\" for=\"{$sub_slug}-some-concern\">some-concern</label>
+                                    <input type=\"radio\" id=\"{$lecture_slug}-{$assignment_slug}-some-concern\" name=\"{$assignment_name}\" value=\"some-concern\">
+                                    <label class=\"better-label concern\" for=\"{$lecture_slug}-{$assignment_slug}-some-concern\">some-concern</label>
                               </div>
                               <div class=\"input-piece\">
-                                    <input type=\"radio\" id=\"{$sub_slug}-confident\" name=\"{$assignment_name}\" value=\"confident\">
-                                    <label class=\"better-label ok\" for=\"{$sub_slug}-confident\">confident</label>
+                                    <input type=\"radio\" id=\"{$lecture_slug}-{$assignment_slug}-confident\" name=\"{$assignment_name}\" value=\"confident\">
+                                    <label class=\"better-label ok\" for=\"{$lecture_slug}-{$assignment_slug}-confident\">confident</label>
                               </div>
                               ";
                   }
