396b2d9553e519f62937191c3ef084a2f2ee38b0
woodwardtw <twwoodward@me.com>
2020-10-24 07:53:38 -0400

    style input elements

--- loop-templates/student-form.php
+++ loop-templates/student-form.php
@@ -5,15 +5,15 @@
                   if(get_sub_field('assignment_name',$sub_post_id) != ""){
                         $assignment_name = get_sub_field('assignment_name',$sub_post_id);
                         echo '<h4>' . $assignment_name . '</h4>';
-                        echo "<div>
+                        echo "<div class=\"input-piece\">
                                     <input type=\"radio\" id=\"{$sub_slug}-need-help\" name=\"{$assignment_name}\" value=\"need-help\">
                                     <label class=\"better-label help\" for=\"{$sub_slug}-need-help\">Need Help</label>
                               </div>
-                              <div>
+                              <div class=\"input-piece\">
                                     <input type=\"radio\" id=\"{$sub_slug}-some-concern\" name=\"{$assignment_name}\" value=\"some-concern\">
                                     <label class=\"better-label concern\" for=\"{$sub_slug}-some-concern\">some-concern</label>
                               </div>
-                              <div>
+                              <div class=\"input-piece\">
                                     <input type=\"radio\" id=\"{$sub_slug}-confident\" name=\"{$assignment_name}\" value=\"confident\">
                                     <label class=\"better-label ok\" for=\"{$sub_slug}-confident\">confident</label>
                               </div>
